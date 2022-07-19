f20d8356f175e318d37abde09279db1e8b23e687
mina-payout <81137895+mina-payout@users.noreply.github.com>
2022-07-19 19:57:29 +0530

    added api for EmbedID token

---
+++ src/main/java/com/minanet/trulioo/kyc/KYCConfiguration.java
@@ -0,0 +1,15 @@
+package com.minanet.trulioo.kyc;
+
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
+public class KYCConfiguration {
+	@Bean
+    @Qualifier("restTemplate")
+	RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+}
